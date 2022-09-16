fac4d1410c9fb1049960374e1fd2ceaa8cc7c60a
DCHamerslag <d.c.hamerslag@gmail.com>
2022-09-16 17:55:37 +0200

    init

---
+++ main.py
@@ -0,0 +1,5 @@
+
+from utils.paths import ROOT, DATA
+
+if __name__ == "__main__":  
+    print(ROOT)
---
+++ utils/paths.py
@@ -0,0 +1,5 @@
+import os
+from pathlib import Path
+
+ROOT = Path(__file__).parent.parent
+DATA = ROOT / "data"
